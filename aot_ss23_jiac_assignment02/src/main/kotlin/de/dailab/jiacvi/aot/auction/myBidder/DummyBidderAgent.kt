414db0bb0638b5bc57b552cc2e4c92802ed1e76f
y_sp3ngl3r <yannik.spengler@campus.tu-berlin.de>
2023-06-15 16:43:45 +0200

    setup-first bidder

--- aot_ss23_jiac_assignment02/src/main/kotlin/de/dailab/jiacvi/aot/auction/myBidder/DummyBidderAgent.kt
+++ aot_ss23_jiac_assignment02/src/main/kotlin/de/dailab/jiacvi/aot/auction/myBidder/DummyBidderAgent.kt
@@ -4,11 +4,12 @@ import de.dailab.jiacvi.aot.auction.*
 import de.dailab.jiacvi.Agent
 import de.dailab.jiacvi.BrokerAgentRef
 import de.dailab.jiacvi.behaviour.act
+import kotlin.system.exitProcess
 
 /**
  * This is a simple stub of the Bidder Agent. You can use this as a template to start your implementation.
  */
-class DummyBidderAgent(private val id: String): Agent(overrideName=id) {
+class DummyBidderAgent(private val id: String) : Agent(overrideName = id) {
     // you can use the broker to broadcast messages i.e. broker.publish(biddersTopic, LookingFor(...))
     private val broker by resolve<BrokerAgentRef>()
 
@@ -17,12 +18,8 @@ class DummyBidderAgent(private val id: String): Agent(overrideName=id) {
     private var secret: Int = -1
 
     override fun behaviour() = act {
-        // TODO implement your bidding strategie. When to offer (buy/sell) items
-        //  at which price. You can also use the cashin function if you need money.
-
-        // goal: sammle möglichst viele Items des gleichen typs mit noch möglichst viel Restgeld über
-        // --> ab 3 item des gleichen types kumulativ
-        //start: poste item mit wenigster Anzahl
+        // easy - Bidder Agent.
+        // buy and sell only received goods with formula: fib(number of good+1) - fib(number of good)
 
         // register to all started auctions
         listen<StartAuction>(biddersTopic) {
@@ -40,16 +37,18 @@ class DummyBidderAgent(private val id: String): Agent(overrideName=id) {
             wallet = Wallet(id, it.items.toMutableMap(), it.credits)
             secret = it.secret
             log.info("Initialized Wallet: $wallet, secret: $secret")
+            bid()
         }
 
         // be notified of result of own offer
         on<OfferResult> {
             log.info("Result for my Offer: $it")
             when (it.transfer) {
-                Transfer.SOLD   -> wallet?.update(it.item, -1, +it.price)
+                Transfer.SOLD -> wallet?.update(it.item, -1, +it.price)
                 Transfer.BOUGHT -> wallet?.update(it.item, +1, -it.price)
                 else -> {}
             }
+            bid()
         }
 
         listen<Digest>(biddersTopic) {
@@ -64,11 +63,27 @@ class DummyBidderAgent(private val id: String): Agent(overrideName=id) {
         on<AuctionResult> {
             log.info("Result of Auction: $it")
             wallet = null
+            exitProcess(0)
         }
 
     }
 
-    private fun getItemOfMinimalNumber(): Item?  {
+    private fun bid() {
+        val ref = system.resolve(auctioneer)
+        if (wallet != null) {
+            for (item in wallet!!.items) {
+                val optimalPrice = fib(item.value + 1) - fib(item.value)
+                if (optimalPrice <= wallet!!.credits) {
+                    log.info("Bidding on Item " + item.key + "with value " + item.value)
+                    ref invoke ask<Boolean>(Offer(id, secret, item.key, optimalPrice.toDouble())) { res ->
+                        log.info("Offer valid: $res")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getItemOfMinimalNumber(): Item? {
         if (wallet != null) {
             var maxItem = Item(-1)
             var maxValue = -1
